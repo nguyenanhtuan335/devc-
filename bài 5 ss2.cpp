3161921b8e2629f0f129fb821a2a834c6f46fed1
nguyenanhtuan335 <nguyenthoithoi0@gmail.com>
2024-11-20 03:47:20 +0700

    Add files via upload

---
+++ bài 5 ss2.cpp
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main(){
+	int chieudai = 5;
+	int chieurong = 10;
+	// chu vi hcn = 2*(chieudai+chieurong)
+	// chu vi hinh chu nhât
+	int chuvi = 2*(chieudai+chieurong); 
+	printf(" chuvi  la %d \n ", chuvi );
+	
+	
+	// tích phân hcn
+	double a=0;
+	// a=5 là can duoi tich phan
+	double b=10;
+	// la can tren tich phan
+	// x^2 = 5^2-10^2
+	int tichphan = 5^2-10^2;
+	printf(" tich phan tren khoang %d\n ",tichphan);
+	
+	
+	
+
+	
+  
+
+   return 0;
+}
