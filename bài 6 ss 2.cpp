735ba47ff17455314ce8631c1d7d8adcf8fd7509
nguyenanhtuan335 <nguyenthoithoi0@gmail.com>
2024-11-20 04:11:17 +0700

    Add files via upload

---
+++ bài 6 ss 2.cpp
@@ -0,0 +1,40 @@
+#include<stdio.h>
+int main(){
+	
+	float pi = 3.14;
+	// ploat pi la hang so cua hinh tron
+	
+	float r = 6.0;
+	// tinh chu vi hinh tron
+	// r : la ban kinh hinh tron
+	// chu vi hinh tron = 2*pi*r
+	float chuvi = 2*pi*r;
+	
+	// tinh dien tich hinh tron = 3.14*r*r
+	float dientich = pi*r*r;
+	
+	printf(" ban kinh hinh tron : %.2f\n", r);
+    printf(" chu vi  hinh tron : %.2f\n", chuvi);
+    printf(" dien ticih hinh tron : %.2f\n", dientich );
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}	
+
+	   
+
+
+
+
+
+
+
