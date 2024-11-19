7c8ca07a1c5b19bd942e526ab7b76138d406f64e
nguyenanhtuan335 <nguyenthoithoi0@gmail.com>
2024-11-20 01:59:02 +0700

    Add files via upload

---
+++ bài 2 ss2.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){
+	int so1 = 10;
+	int so2 = 20;
+	
+	// toan tu +
+	int tong = so1 + so2; 
+	printf("Phep tong la %d \n ", tong );
+	
+  
+   return 0;
+}
+   
+   
+
+
+
+
+   
