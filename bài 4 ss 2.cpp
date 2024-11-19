62a580f701bf96cbd6ff4c8d8a9dfe4fc7dde352
nguyenanhtuan335 <nguyenthoithoi0@gmail.com>
2024-11-20 03:04:10 +0700

    Add files via upload

---
+++ bài 4 ss 2.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(){
+	int side = 10;
+	// side là 1 canh hinh vuong gán là 10 
+	int total = 4;
+	// chu vi hình vuông là= side*total 
+	// toan tu chu vi
+	int chuvi = side * total ; 
+	printf("phep chu vi la %d \n ", chuvi  );
+	
+	// dien tich hinh vuong
+    // gán side là canh hinh vuông
+    // dien tich hình vuong = side*side
+    int dientich=side * side;
+    printf("phep dien tich la %d\n", dientich);
+  
+ return 0;
+}
