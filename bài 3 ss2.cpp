b217db7d178ab86a01a075ce37431a2a0cb7d97f
nguyenanhtuan335 <nguyenthoithoi0@gmail.com>
2024-11-20 02:05:37 +0700

    Add files via upload

---
+++ bài 3 ss2.cpp
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main(){
+	int so1 = 10;
+	int so2 = 20;
+	
+	// toan tu -
+	int hieu = so1 - so2; 
+	printf("Phep hieu  la %d \n ", hieu );
+	
+    // toan tu * 
+	int tich = so1 * so2; 
+	printf("Phep tich la %d \n ", tich );
+	
+	// toan tu /
+	int thuong = so1 / so2; 
+	printf("Phep thuong la %d \n ", thuong  );
+	
+  
+
+   return 0;
+}
+   
+   
+
+
+
+
+   
